d98b06e30ccb9e6e53a37aafc655a97d87cecaa6
Jim Lawruk <c-jlawruk@pa.gov>
2024-04-23 09:40:08 -0400

    Initial blank map

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
+import Locate from "@arcgis/core/widgets/Locate.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const map = new Map({
+  basemap: "topo-vector",
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const view = new MapView({
+  container: "mapViewDiv",
+  map: map,
+  zoom: 8,
+  center: [-78, 41],
+});
+
+var toggle = new BasemapToggle({
+  view: view,
+});
+view.ui.add(toggle, "bottom-right");
+
+var locateBtn = new Locate({
+  view: view,
+});
+view.ui.add(locateBtn, "top-left");
+
+
